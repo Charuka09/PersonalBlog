85d83c0b99ba59516efc4193ce9ca0f0cd947186
charuka herath <charuka09@gmail.com>
2019-05-16 18:14:42 +0530

    Updates

---
+++ precache-manifest.d46dd141e42c089c7787f0a110f2dacd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffd13a4ae9cb388ccd8e",
+    "url": "/Charuka09/PersonalBlog/static/js/runtime~main.ffd13a4a.js"
+  },
+  {
+    "revision": "58b5a27c8154e0031e9a",
+    "url": "/Charuka09/PersonalBlog/static/js/main.58b5a27c.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Charuka09/PersonalBlog/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "58b5a27c8154e0031e9a",
+    "url": "/Charuka09/PersonalBlog/static/css/main.e6b476d5.chunk.css"
+  },
+  {
+    "revision": "a9b1c8362d81854f38190d6bbee6f22a",
+    "url": "/Charuka09/PersonalBlog/index.html"
+  }
+];
